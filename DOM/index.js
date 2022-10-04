6de186f3886b26282453c0bbc5ce015c3adb6e31
bogdan <zuyrinbogdank@gmail.com>
2022-10-04 21:19:48 +0300

    dom2

--- DOM/index.js
+++ DOM/index.js
@@ -96,16 +96,80 @@
 
 // list.addEventListener('click', onLiClick);
 
-const btn = document.getElementById('btn');
 
-function onBtnClick () {
-    let result = Math.ceil(Math.random() * 3);
-    if (result = 1) {
-        console.log('Shara');
-    } else if (result = 2) {
-        console.log ('Shara');
-    } else if (result = 3) {
-        console.log ('BOOM');
-    }
-}
-btn.addEventListener('click', onBtnClick);
+// const wrapper = document.getElementById('wrapper');
+// const list = document.getElementById('list');
+// const firstItem = document.getElementById('fitem');
+// const pItem = document.getElementById('pitem');
+
+// wrapper.addEventListener('click', () => {
+//     console.log('WRAPPER')
+// });
+
+// list.addEventListener('click', () => {
+//     console.log('LIST')
+// });
+
+// firstItem.addEventListener('click', () => {
+//     console.log('LI')
+// });
+
+// pItem.addEventListener('click', () => {
+//     console.log('P')
+// });
+
+// для перехвата добавляем true после функции, stopPropagation() останавливает всплытие и перехват
+
+
+// list.addEventListener('click', () => {
+    // const newLi = document.createElement('li');
+    // newLi.innerText = 'some text here';
+
+    // const item3 = list.getElementsByTagName('li')[2];
+
+    // list.append(item3.cloneNode(true));
+
+    // list.insertBefore(newLi, bratik );
+
+    // list.append(newLi);
+    // list.prepend(newLi);
+    // list.insertAdjacentElement('afterbegin', newLi);
+    // list.insertAdjacentElement('afterend', newLi);
+    // list.insertAdjacentElement('beforebegin', newLi);
+    // list.insertAdjacentElement('beforeend', newLi);
+
+    // list.insertAdjacentHTML('afterbegin', '<h1>Hello</h1>');
+    // list.insertAdjacentText('afterbegin', '<h1>Hello</h1>');
+
+    // list.parentElement.append();
+
+    // list.after(newLi);
+// });
+
+
+// document.getElementById('pitem').remove();
+
+// pItem.textContent = '<h1>Hello</h1>';
+
+// pItem.addEventListener('click', () => {
+    // pItem.setAttribute('wasClicked', 'true');
+    // console.log(pItem.getAttribute('wasClicked'));
+    // const pItemCopy = pItem.cloneNode(true);
+    // console.log(pItemCopy);
+    // console.dir(pItem);
+    // pItem.dataset.test = 'ppppppp';
+    // Object.keys(pItem.dataset).forEach()
+
+    // console.dir();
+
+    // pItem.remove();
+    // console.log(pItem);
+// });
+
+
+const pItem = document.getElementById('pitem');
+const list = document.getElementById('list');
+
+pItem.addEventListener('click', () => {
+    pItem.style.backgroundColor = 'red';
+});
